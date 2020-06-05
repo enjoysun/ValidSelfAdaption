8c601ffc0b46f6f57480e015bba4a561fde91f7e
myou <myoueva@gmail.com>
2020-06-05 15:41:50 +0800

    init

---
+++ src/main/java/com/myou/validadpation/ValidAdpationApplication.java
@@ -0,0 +1,13 @@
+package com.myou.validadpation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidAdpationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidAdpationApplication.class, args);
+    }
+
+}
